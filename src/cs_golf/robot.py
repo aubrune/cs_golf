05035ee69a9e7bbff5fafaf58a9c1831b86dd77e
Yoan Mollard <ymollard@users.noreply.github.com>
2019-05-09 01:24:25 +0200

    Allow to display trajs

--- src/cs_golf/robot.py
+++ src/cs_golf/robot.py
@@ -1,7 +1,8 @@
 import rospy
 import moveit_commander
 from sensor_msgs.msg import JointState
-from moveit_msgs.msg import RobotState
+from moveit_msgs.msg import RobotState, DisplayTrajectory, RobotTrajectory
+from trajectory_msgs.msg import JointTrajectory
 from cs_golf.trajectories import trapezoidal_speed_trajectory
 from cs_golf.persistence import dicttostate
 
@@ -15,6 +16,7 @@ class Robot(object):
 
         self.commander = moveit_commander.RobotCommander()
         self.group = moveit_commander.MoveGroupCommander(group_name)
+        self._display_pub = rospy.Publisher("/{}/move_group/display_planned_path".format(ns), DisplayTrajectory, queue_size=1)
 
     @property
     def current_state(self):
@@ -22,6 +24,17 @@ class Robot(object):
 
     def execute(self, *args, **kwargs):
         return self.group.execute(*args, **kwargs)
+
+    def display(self, trajectory):
+        if isinstance(trajectory, RobotTrajectory):
+            trajectory = trajectory.joint_trajectory
+        if not isinstance(trajectory, JointTrajectory):
+            rospy.logerr("robot.display() only accepts joint trajectories")
+            return
+        dt = DisplayTrajectory()
+        rt = RobotTrajectory(joint_trajectory = trajectory)
+        dt.trajectory.append(rt)
+        self._display_pub.publish(dt)
 
     def go(self, goal_state, kv_max=1.0, ka_max=0.5, wait=True):
         if isinstance(goal_state, dict):
