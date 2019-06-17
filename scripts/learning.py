c4e2f329afb4e026795f62e2836aa3efc4d08399
Yoan Mollard <ymollard@users.noreply.github.com>
2019-06-18 01:35:46 +0200

    Learning OK with poping CV window

--- scripts/learning.py
+++ scripts/learning.py
@@ -6,22 +6,36 @@ import rospkg
 import json
 import numpy as np
 from os.path import join
+from cv_bridge import CvBridge
 from moveit_msgs.msg import RobotTrajectory
 from trajectory_msgs.msg import JointTrajectoryPoint
 from cs_golf.srv import RateIteration, RateIterationResponse
 from cs_golf.srv import Plan, PlanResponse
+from sensor_msgs.msg import Image
 from cs_golf.persistence import dicttostate
 
-
 class Learning(object):
+    NUM_VALUES = 100   # Number of discrete values from each exploration space (angle + speed)
+    DURATION_RANGE = [0.15, 0.2]    # Minimum and maxium durations of motions ; speed indexes are mapping within this range
+                                    # Acceleration and deceleration of 25% are added to this duration 
+
     def __init__(self):
+        self.bridge = CvBridge()
         self.smoke_motion_id = 0
+        self.scores = np.ones((self.NUM_VALUES, self.NUM_VALUES))  # scores[speed_i][angle_i] = mark of this trajectory between [0., 1.]
+        self.num_votes = 0  # Number of votes taken into account in self.scores so far
+        self.pending_iteration_parameters = [-1, -1, -1]  # iteration + speed + angle parameters indexes to be scored
+        self.x = np.linspace(0, 1, self.NUM_VALUES)   # Basic linear space for speed and angle dimensions
         self.rospack = rospkg.RosPack()
         rospy.Service('golf/learning/plan', Plan, self._cb_plan)
         rospy.Service('golf/learning/rate', RateIteration, self._cb_rate)
+        self.scores_pub = rospy.Publisher("golf/learning/scores", Image, queue_size=1)
 
         with open(join(self.rospack.get_path("cs_golf"), "config/motions.json")) as f:
             self.motions = json.load(f)
+
+        with open(join(self.rospack.get_path("cs_golf"), "config/optimal.json")) as f:
+            self.optimal = json.load(f)
        
         rospy.loginfo("Learning node is ready to plan!")
 
@@ -49,25 +63,104 @@ class Learning(object):
             rt.joint_trajectory.points.append(jtp)
         return rt
 
+    def _pick_trajectories_params(self, iteration):
+        """
+        Generates parameters of trajectories:
+          * Angle (motion ID)
+          * Speed (mapped to trajectory duration in seconds later on)
+        
+        It is influenced by 3 components:
+          * User's grade given to the last iteration between 0 and 10
+          * Randomness
+          * Natural attraction to the optimal parameters
+        """
+        mark_ratio = float(iteration)/self.optimal["num_iterations_convergence"]
+        # TODO: Should we force convergence to a right value?
+        optimal_ratio = 0 # 2./self.optimal["num_iterations_convergence"]  
+        rand_ratio = 1. - mark_ratio - optimal_ratio
+
+        # Highest mark component
+        highest_mark_speed_i, highest_mark_angle_i =  np.unravel_index(np.argmax(self.scores), self.scores.shape)
+
+        # Random component
+        random_speed_i = np.random.randint(0, self.NUM_VALUES)
+        random_angle_i = np.random.randint(0, self.NUM_VALUES)
+
+        # TODO: Should we force convergence to a right value?
+        optimal_speed_i = self.optimal["optimal_speed_i"]
+        optimal_angle_i = self.optimal["optimal_angle_i"]
+
+        # Merging random and highest mark components
+        selected_speed_i = int(random_speed_i*rand_ratio + highest_mark_speed_i*mark_ratio + optimal_speed_i*optimal_ratio)
+        selected_angle_i = int(random_angle_i*rand_ratio + highest_mark_angle_i*mark_ratio + optimal_angle_i*optimal_ratio)
+
+        return selected_speed_i, selected_angle_i
+
+    def add_score(self, mark, value_speed, value_angle):
+        def gauss(x, mu, sigma):
+            return np.exp(-(x-mu)*(x-mu)/(2*sigma*sigma))
+        
+        SIGMA = 0.1   # Impact of a single mark on neighbours
+        speeds = np.tile((mark)*gauss(self.x, value_speed, SIGMA), [self.NUM_VALUES,1])
+        angles = np.tile((mark)*gauss(self.x, value_angle, SIGMA).reshape((self.NUM_VALUES, 1)), self.NUM_VALUES)
+        new_frame = np.multiply(speeds, angles)
+        self.scores = (self.num_votes*self.scores + new_frame)/(self.num_votes+1)
+        self.num_votes += 1
+
     def _cb_plan(self, req):
-        i_motion = 0
+        iteration = rospy.get_param("golf/iteration")
+        i_motion, i_speed = self._pick_trajectories_params(iteration)
+
+        # If smoke mode is active, overide the planned trajectory
         if rospy.get_param('golf/smoke', False):
             i_motion = self.smoke_motion_id
             rospy.logwarn("Planning the smoke trajectory #{}".format(i_motion))
             self.smoke_motion_id = (self.smoke_motion_id + 1) % len(self.motions["trajectories"])
-        duration = 0.2
+        
+        duration = i_speed*(self.DURATION_RANGE[1] - self.DURATION_RANGE[0])/self.NUM_VALUES + self.DURATION_RANGE[0]   # Linear mapping between indexes and min/max trajecotry durations
         angle = self.motions["trajectories"][i_motion]["angle"]
         traj = self._make_shooting_trajectory(self.motions["trajectories"][i_motion]["points"], duration)
-        rospy.loginfo("Generated a trajectory of {} sec with angle {}".format(traj.joint_trajectory.points[-1].time_from_start.to_sec(), angle))
+        rospy.loginfo("Generated a trajectory of {} sec with angle {} (motion index {})".format(traj.joint_trajectory.points[-1].time_from_start.to_sec(), angle, i_motion))
+        self.pending_iteration_parameters[0] = iteration
+        self.pending_iteration_parameters[1] = i_speed     # Speed index
+        self.pending_iteration_parameters[2] = i_motion    # Motion's ID = its angle (index)    
         return PlanResponse(trajectory=traj)
 
     def _cb_rate(self, req):
-        response = RateIterationResponse()
+        speed_i = self.pending_iteration_parameters[1]
+        angle_i = self.pending_iteration_parameters[2]
+        iteration = self.pending_iteration_parameters[0]
+        success = False
+        try:
+            scored_iteration = int(req.iteration)
+            int_grade = int(req.grade)
+        except:
+            scored_iteration = -1
+            int_grade = -1
+        else:
+            if speed_i >= 0 and angle_i >= 0 and iteration >= 0 and iteration == scored_iteration:
+                if 0 <= int_grade <= 10:
+                    float_grade = int_grade/10.
+                    rospy.logwarn("Recording mark {} for speed index {} and angle index {}".format(float_grade, speed_i, angle_i))
+                    self.add_score(float_grade, float(speed_i)/self.NUM_VALUES, float(angle_i)/self.NUM_VALUES)
+                    success = True
+                    self.pending_iteration_parameters = [-1, -1, -1]
+                else:
+                    rospy.logerr("Invalid integer grade not in [0, 10]: {}".format(int_grade))
+            else:
+                rospy.logerr("Unexpected score for iteration {}".format(scored_iteration))
+            response = RateIterationResponse(success=success)
         return response
 
     def run(self):
-        rospy.spin()
-
+        rate = rospy.Rate(0.1)
+        while not rospy.is_shutdown():
+            #image = self.bridge.cv2_to_imgmsg(self.scores, encoding="8UC1")
+            #self.scores_pub.publish(image)
+            import matplotlib.pyplot as plt
+            plt.imshow(self.scores, cmap='hot', interpolation='nearest')
+            plt.show()
+            rate.sleep()
 
 if __name__=='__main__':
     rospy.init_node('cs_golf_learning')
--- scripts/user.py
+++ scripts/user.py
@@ -13,12 +13,15 @@ from threading import Thread
 from copy import copy
 from std_msgs.msg import Bool
 from std_srvs.srv import Trigger
+from cs_golf.srv import RateIteration, RateIterationRequest
+from logging import getLogger, INFO
 
 class UserNode(object):
     def __init__(self):
         self.rospack = RosPack()
         self.port = 80 if os.getuid() == 0 else 5000
         self.app = Flask(__name__)
+        getLogger(__name__).setLevel(INFO)
         #self.cors = CORS(self.app, resources={r'/api/*': {'origins': '*'}})
         self.app.route('/ready', methods=['GET'])(self.get_ready)
         self.app.route('/go', methods=['POST'])(self.post_go)
@@ -35,21 +38,40 @@ class UserNode(object):
     @property
     def commanding(self):
         now = rospy.Time.now()
-        return self._last_commanding_status and self._last_commanding_status_date > now - rospy.Duration (0.5)
+        return (self._last_commanding_status and self._last_commanding_status_date > now - rospy.Duration (0.5)) or rospy.get_param("golf/simulated", False)
 
     def get_ready(self):
+        """
+        Tells if the robot is ready to triger a new iteration
+        Warning: "ready" from the REST API stands for "Robot is commanding + LIDAR OK + LIDAR view free + no interaction in progress"
+                  while "golf/ready" in parameter server stands for "interaction controller is not ready" only
+        """
         try:
             iteration = rospy.get_param("golf/iteration", 0)
             lidar_ok = not rospy.get_param("golf/lidar_error", True)
             ready = rospy.get_param("golf/ready", False)
             terrainBusy = rospy.get_param("golf/terrainBusy", True)
             all_ready = ready and lidar_ok and not terrainBusy
+            simulated = rospy.get_param("golf/simulated", False)
         
+            message = []
+            if not lidar_ok:
+                message.append("LIDAR is not working.")
+            if not self.commanding:
+                message.append("Robot is not in COMMANDING state.")
+            if not ready:
+                message.append("Interaction controller does not report it's ready.")
+            if terrainBusy:
+                message.append("Terrain is currently busy or LIDAR is obstructed.")
+            if simulated:
+                message.append("Robot setup is simulated.")
+
             return json.dumps({
                 "working": self.commanding and lidar_ok,
                 "ready": all_ready,
                 "terrainBusy": terrainBusy,
-                "iteration": iteration
+                "iteration": iteration,
+                "message": " ".join(message)
             })
         except:
             return json.dumps({
@@ -111,37 +133,33 @@ class UserNode(object):
             })
 
     def post_rate(self):
-        def service_rate():
+        def service_rate(int_grade=9999, iteration=9999):
             try:
-                rospy.wait_for_service('golf/rate', 0.5)
-                response = rospy.ServiceProxy('golf/rate', Trigger)()
+                rospy.wait_for_service('golf/learning/rate', 0.5)
+                response = rospy.ServiceProxy('golf/learning/rate', RateIteration)(grade=int_grade, iteration=iteration)
                 return response.success
-            except (rospy.ServiceException, rospy.ROSException) as e:
+            except Exception, e:
                 rospy.logerr("Cannot call service to rate iteration: " + repr(e))
+            rospy.logerr("Service 'golf/learning/rate timedout")
             return False
 
         try:
             data = request.get_json()
             if data is not None and "iteration" in data and "note" in data:
-                iteration = rospy.get_param("golf/iteration", 0)
-                note = int(data["note"])
-                print("NOTE=", note)
-                received_iteration = int(data["iteration"])
-                expected_rated_iteration = iteration - 1 # We're rating the previous iteration
-                if received_iteration == expected_rated_iteration:
-                    success = service_rate()
-                    success = True
-                    return json.dumps({"success": success})
-                else:
-                    return json.dumps({
-                        "success": True,
-                        "message": "Iteration mismatch, expecting {}, got {}".format(expected_rated_iteration, data["iteration"])
-                    })
+                try:
+                    note = int(data["note"])
+                    iteration = int(data["iteration"])
+                except:
+                    note = 9999  # Will trigger error
+                    iteration = 9999
+                success = service_rate(note, iteration)
+                return json.dumps({"success": success})
             return json.dumps({
                 "success": False,
                 "message": "Request has not a valid JSON payload"
             })
-        except:
+        except Exception as e:
+            rospy.logerr(repr(e))
             return json.dumps({
                 "success": False,
                 "message": "No service"
